84367ac48ac08e207fa3101b4a3f7744d7844614
kei-en <njuguna0karanja@gmail.com>
2024-01-03 20:54:01 +0300

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((result) => {
+      const resultBody = result[0].body;
+      const resultfirstName = result[1].firstName;
+      const resultlastName = result[1].lastName;
+
+      console.log(`${resultBody} ${resultfirstName} ${resultlastName}`);
+    })
+    .catch((error) => {
+      console.log('Signup system offline', error);
+    });
+}
