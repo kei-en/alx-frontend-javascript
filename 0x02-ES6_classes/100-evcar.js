c9d1e14db451a9dce552c8dd6f22ea1e47d3de73
kei-en <njuguna0karanja@gmail.com>
2024-01-05 13:56:09 +0300

    EVCar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,13 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    // eslint-disable-next-line no-underscore-dangle
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
